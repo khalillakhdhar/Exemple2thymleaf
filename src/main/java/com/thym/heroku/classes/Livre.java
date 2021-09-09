8ad68ea4e28091db3ea223cd27d768aa198180e0
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 14:25:40 +0100

    codesync

--- src/main/java/com/thym/heroku/classes/Livre.java
+++ src/main/java/com/thym/heroku/classes/Livre.java
@@ -1,5 +1,40 @@
 package com.thym.heroku.classes;
 
 public class Livre {
+private String titre,auteur;
+private int prix;
+
+public String getTitre() {
+	return titre;
+}
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+public String getAuteur() {
+	return auteur;
+}
+public void setAuteur(String auteur) {
+	this.auteur = auteur;
+}
+public int getPrix() {
+	return prix;
+}
+public void setPrix(int prix) {
+	this.prix = prix;
+}
+public Livre(String titre, String auteur, int prix) {
+	this.titre = titre;
+	this.auteur = auteur;
+	this.prix = prix;
+}
+@Override
+public String toString() {
+	return "Livre [titre=" + titre + ", auteur=" + auteur + ", prix=" + prix + "]";
+}
+
+
+
+
+
 
 }
--- src/main/java/com/thym/heroku/controllers/MainController.java
+++ src/main/java/com/thym/heroku/controllers/MainController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.thym.heroku.classes.Livre;
 
 @Controller
 public class MainController {
@@ -20,5 +21,18 @@ public String liste(Model mod, @RequestParam String nom)
 {
 mod.addAttribute("name",nom);
 return "affichage";
+}
+@GetMapping("/livre")
+public String livres()
+{
+return "livres";	
+}
+@PostMapping("/livre")
+public String create(Model m, @RequestParam String titre,String auteur,int prix)
+{
+Livre l=new Livre("teste","je teste",20);
+m.addAttribute("livre",l.toString());
+return "livres";
+
 }
 }
