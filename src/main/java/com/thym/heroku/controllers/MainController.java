56076a69111f8f5bf19a435760da51803f629a3c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 13:50:15 +0100

    update

--- src/main/java/com/thym/heroku/controllers/MainController.java
+++ src/main/java/com/thym/heroku/controllers/MainController.java
@@ -3,7 +3,9 @@ package com.thym.heroku.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -13,5 +15,10 @@ public String accueil(Model m)
 m.addAttribute("message","bienvenu");
 return "index";
 }
-
+@PostMapping("")
+public String liste(Model mod, @RequestParam String nom)
+{
+mod.addAttribute("name",nom);
+return "affichage";
+}
 }
